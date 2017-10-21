c9d9c8068c64458c326d4a8a4a66dfcb7b9e2b3d
Vivek Vishwakarma <vishwakarmavivek83@gmail.com>
2017-10-21 18:40:57 +0530

    changed UI of test page

--- js/give-test.js
+++ js/give-test.js
@@ -3,6 +3,8 @@ var tests = [];
 var i =-1;
 var qid;
 var option_selected;
+var current_question = 1;
+var last_question = 1;
 function give_test(professor,test)
 {
   var xmlhttp = new XMLHttpRequest();
@@ -12,15 +14,19 @@ function give_test(professor,test)
         {
         i++;
         tests=JSON.parse(this.responseText);
-        document.getElementById("question-paragraph").innerHTML = "<p >"+tests[i]['question']+"</p>";
+        document.getElementById("question-paragraph").innerHTML = "<p ><h1>"+tests[i]['question']+"</h1></p>";
         var opt = tests[i]['options'].split(',');
         for(var j = 0;j<opt.length;j++)
-          document.getElementById("options").innerHTML += "<input type='radio' name='option' id="+j+" value="+j+" >"+opt[j]+"<br/>";
-        }
+          document.getElementById("options").innerHTML += "<input type='radio' name='option' id="+j+" value="+j+" ><div class='opts'>"+opt[j]+"</div><br>";
+
         for (var k = 0; k < tests.length; k++) {
           document.getElementById("questions").innerHTML += "<a href='#' class='list-group-item' id=question"+tests[k]['qno']+" onclick=' question_list("+k+"); '>"+(k+1)+".  "+tests[k]['question']+"</a> "
         }
-      
+
+        document.getElementById('question'+current_question).style.backgroundColor="White";
+        document.getElementById('question'+current_question).style.color="black";
+      }
+
     };
   xmlhttp.open("GET","giveTest.php?test="+test, true);
   xmlhttp.send();
@@ -39,17 +45,26 @@ function next()
           return;
       }
 
-        
-      
+
+
             i++;
-            document.getElementById("question-paragraph").innerHTML = "<p>"+tests[i]['question']+"</p>";
+            current_question =i+1;
+            document.getElementById("question-paragraph").innerHTML = "<p><h1>"+tests[i]['question']+"</h1></p>";
             var opt1 = tests[i]['options'].split(',');
             document.getElementById("options").innerHTML = "";
             for(var j = 0;j<opt1.length;j++)
-            document.getElementById("options").innerHTML += "<input type='radio' name='option' id="+j+" value="+j+" >"+opt1[j]+"<br/>";
+            document.getElementById("options").innerHTML += "<input type='radio' name='option' id="+j+" value="+j+" ><div class ='opts'>"+opt1[j]+"</div><br/>";
+
+            document.getElementById('question'+last_question).style.backgroundColor="#5f5f5f";
+            document.getElementById('question'+last_question).style.color="white";
+            document.getElementById('question'+current_question).style.backgroundColor="White";
+            document.getElementById('question'+current_question).style.color="black";
+
+            last_question = current_question;
+
           check_option();
-          
-      
+
+
 }
 
 
@@ -71,15 +86,25 @@ function previous()
         else
           {
           i--;
-          document.getElementById("question-paragraph").innerHTML = "<p>"+tests[i]['question']+"</p>";
+          current_question = i+1;
+          document.getElementById("question-paragraph").innerHTML = "<p><h1>"+tests[i]['question']+"</h1></p>";
           var opt2 = tests[i]['options'].split(',');
           document.getElementById("options").innerHTML = "";
           for(var j = 0;j<opt2.length;j++)
-            document.getElementById("options").innerHTML += "<input type='radio' name='option' id="+j+" value="+j+" >"+opt2[j]+"<br/>";
+            document.getElementById("options").innerHTML += "<input type='radio' name='option' id="+j+" value="+j+" ><div class='opts'>"+opt2[j]+"</div><br/>";
+
+
+            document.getElementById('question'+last_question).style.backgroundColor="#5f5f5f";
+            document.getElementById('question'+last_question).style.color="white";
+            document.getElementById('question'+current_question).style.backgroundColor="White";
+            document.getElementById('question'+current_question).style.color="black";
+
+            last_question = current_question;
           }
+
           check_option();
 
-        }
+}
 
 
 
@@ -96,14 +121,14 @@ function previous()
 function check_option(){
   var xmlhttp = new XMLHttpRequest();
 
-    
+
       xmlhttp.onreadystatechange = function()
       {
         if (this.readyState == 4 && this.status==200)
         {
           console.log(this.responseText);
           var selected = JSON.parse(this.responseText);
-          
+
 
           if (selected["q"+tests[i]['qno']] != 'undefined' && selected["q"+tests[i]['qno']] != null) {
             console.log("checked");
@@ -113,7 +138,7 @@ function check_option(){
           }
         }
 
-      
+
     };
     xmlhttp.open("GET","selected-options.php?show=?", true);
     xmlhttp.send();
@@ -147,7 +172,7 @@ function save()
 
   var xmlhttp = new XMLHttpRequest();
 
-    
+
       xmlhttp.onreadystatechange = function()
       {
         if (this.readyState == 4 && this.status==200)
@@ -155,7 +180,7 @@ function save()
           // var a =JSON.parse(this.responseText);
         }
 
-      
+
     };
     xmlhttp.open("GET","selected-options.php?question_id="+qid+"&select="+option_selected, true);
     xmlhttp.send();
@@ -168,16 +193,24 @@ function question_list(index){
           option_checked();
           save();
           i = index;
-          document.getElementById("question-paragraph").innerHTML = "<p>"+tests[i]['question']+"</p>";
+          current_question = i+1;
+          document.getElementById("question-paragraph").innerHTML = "<p><h1>"+tests[i]['question']+"</h1></p>";
           var opt2 = tests[i]['options'].split(',');
           document.getElementById("options").innerHTML = "";
           for(var j = 0;j<opt2.length;j++)
-            document.getElementById("options").innerHTML += "<input type='radio' name='option' id="+j+" value="+j+" >"+opt2[j]+"<br/>";
-          
+            document.getElementById("options").innerHTML += "<input type='radio' name='option' id="+j+" value="+j+" ><div class='opts'>"+opt2[j]+"</div><br/>";
+
+
+            document.getElementById('question'+last_question).style.backgroundColor="#5f5f5f";
+            document.getElementById('question'+last_question).style.color="white";
+            document.getElementById('question'+current_question).style.backgroundColor="White";
+            document.getElementById('question'+current_question).style.color="black";
+
+            last_question = current_question;
           check_option();
 
         }
-    
+
 
 
 //function tick(qid)
@@ -191,10 +224,10 @@ function question_list(index){
 //        document.getElementById(qid.id).innerHTML +="<span class='glyphicon glyphicon-ok' style='float:right'></span>"}
 ////    alert(document.getElementById(qid.id).innerHTML);
 //
-//  
+//
 //}
 
 
 function submit(){
-  
+
 }
