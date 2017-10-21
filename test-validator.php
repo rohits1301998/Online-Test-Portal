9ef38e9db235dd68b59b5e8f0b7181bffc109df0
Vivek Vishwakarma <vishwakarmavivek83@gmail.com>
2017-10-22 00:45:03 +0530

    Result page changed and validation working fine

--- test-validator.php
+++ test-validator.php
@@ -4,38 +4,90 @@
 session_start();
 include("conn.php");
 $query = "select username,qno,coption from forms where fnumber=".$_GET['test'];
+$test_title = "select title from gen_forms where fnumber=".$_GET['test'];
 $correct_options = mysqli_query($con,$query);
+$test = mysqli_query($con,$test_title);
+
 $correct=array();
+$marks = array();
+$professor="";
 $score=0;
 while($row = mysqli_fetch_assoc($correct_options))
     {
       $correct['q'.$row['qno']] = $row['coption'];
+      $professor = $row['username'];
     }
 foreach($correct as $x =>$x_value)
 {
   if(array_key_exists($x,$_SESSION['selected-ans']))
   {
     if($correct[$x] == $_SESSION['selected-ans'][$x])
-      $score++;
+      {
+        $score++;
+        $marks[$x] = 1;
+      }
+    else
+      $marks[$x]=0;
   }
+  else
+    $marks[$x] = 0;
 
 }
-echo "your score is : ".$score;
-unset($_SESSION['selected-ans']);
-$_SESSION['exam_over']=1;
+
 
 ?>
 <html>
+<head>
+  <title>Result</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
 <body>
-  <table border="1" cellspacing="0" width="100%;">
+  <div class="container">
+
+  <table class="table table-bordered">
     <tr>
-      <td colspan="2"><p>Profesor Name: </p></td>
+      <td colspan="2"><p>Professor Name: <?php echo $professor; ?></p></td>
+    </tr>
+    <tr>
+      <td colspan="2"><p>Test Name: <?php $t=mysqli_fetch_assoc($test); echo $t['title']."<br />";?></p></td>
+    </tr>
+  </table>
+  <br /><br />
+  <table class="table table-bordered">
+
+    <?php
+    $i=1;
+    foreach ($marks as $key => $value) {
+      if ($value == 1) {
+        echo "<tr><td>Question ".$i++."</td><td><span class='glyphicon glyphicon-ok'></span></td></tr>";
+      }
+      else {
+        echo "<tr><td>Question ".$i++."</td><td><span class='glyphicon glyphicon-remove'></span></td></tr>";
+      }
+    }
+    ?>
+  </table>
+
+  <table class="table table-bordered">
+    <tr>
+      <td>
+        Correct Answer's : <?php echo $score; ?>
+      </td>
     </tr>
     <tr>
       <td>
-        jdsfs
+        Wrong Answer's : <?php echo sizeof($marks)-$score; ?>
       </td>
     </tr>
   </table>
+</div>
 </body>
 </html>
+<?php
+unset($_SESSION['selected-ans']);
+$_SESSION['exam_over']=1;
+?>
